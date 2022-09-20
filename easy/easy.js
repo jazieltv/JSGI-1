08112163f523e6f8d2d4da9462e9dcfbdade38eb
jazieltv <jazieltoribio@gmail.com>
2022-09-20 09:45:48 -0400

    Added HTML file & Created HTML skeleton

---
+++ easy/easy.js
@@ -0,0 +1,8 @@
+var firstName = "Jaaziel";
+var secondName = "Noe";
+let firstNameLength = firstName.length;
+let secondNameLength = secondName.length;
+var characterAmount = firstNameLength - secondNameLength;
+console.log(
+  `The name ${firstName} is longer than the name ${secondName} by ${characterAmount} character(s)`
+);
---
+++ very easy/veryeasy.js
@@ -0,0 +1,3 @@
+var x = 5;
+var y = 9;
+console.log(x - y);
